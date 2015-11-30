b9e7af6e867294d2c53bf8c0d42b6a3f9a0b43c5
--global <--global>
2015-11-30 05:39:01 -0200

    primeiro

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Sistemas de folga</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+		<meta name="author" content="Rafael Dimas">
+		<meta name="description" content="Sistema de Folgas"/>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+	</head>
+	<body>
+		<div class="body">
+			<main class="main">
+				<figure class="logo">
+					<img src="img/logo1.png" alt="Domino's Pizzaria">
+				</figure>
+				<form action="php/login.php" method="post">
+					<input class="usuario" name="usuario" type="text" value="Insira o Usuario aqui">
+					<br><br>
+					<input class="senha" name="senha" type="password" value="Insira">
+					<br><br>
+					<input class="entrar" name="entrar" type="submit" value="Entrar">
+				</form>
+			</main>
+		</div>
+	</body>
+</html>
